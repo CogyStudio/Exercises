50054eef35285f7b593437dc41455e17bd501d8e
CogyStudio <150515822+CogyStudio@users.noreply.github.com>
2024-07-03 17:05:09 +0300

    Exercise08_10 is completed

---
+++ Exercise08_10/MyClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08_10
+{
+    internal class MyClass
+    {
+        public char symb;
+        public static String operator +(MyClass obj1, MyClass obj2)
+        {
+            String result = obj1.symb.ToString() + obj2.symb.ToString();
+            return result;
+        }
+        public static int operator -(MyClass obj1, MyClass obj2)
+        {
+            if (obj1.symb > obj2.symb)
+            {
+                int result = obj1.symb - obj2.symb;
+                return result;
+            }
+            else
+            {
+                int result = obj2.symb - obj1.symb;
+                return result;
+            }
+        }
+        public static implicit operator MyClass(String str)
+        {
+            MyClass result = new MyClass();
+            result.symb = str[0];
+            return result;
+        }
+        public static implicit operator MyClass(int num)
+        {
+            MyClass result = new MyClass();
+            result.symb = (char)num;
+            return result;
+        }
+    }
+}
---
+++ Exercise08_10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj1 = new MyClass();
+            obj1.symb = 'a';
+            MyClass obj2 = new MyClass();
+            obj2.symb = 'd';
+            obj1 -= obj2;
+            Console.WriteLine(obj1 - obj2);
+            Console.WriteLine(obj1.symb);
+        }
+    }
+}
