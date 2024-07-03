4b8c6fd73d173f62925895ee8d9a9e939c9911b3
CogyStudio <150515822+CogyStudio@users.noreply.github.com>
2024-07-03 12:30:39 +0300

    First Commit

---
+++ Exercise08_01/MyClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08_01
+{
+    internal class MyClass
+    {
+        char symb;
+
+        public static MyClass operator++(MyClass obj)
+        {
+            obj.symb = (char)(obj.symb + 1);
+            return obj;
+        }
+        public static MyClass operator--(MyClass obj)
+        {
+            obj.symb = (char)(obj.symb - 1);
+            return obj;
+        }
+        public static MyClass operator+(MyClass obj, int num)
+        {
+            MyClass newObj = new MyClass();
+            newObj.symb = (char)(obj.symb + num);
+            return newObj;
+        }
+        public static int operator-(MyClass obj1, MyClass obj2)
+        {
+            return 41;
+        }
+    }
+}
