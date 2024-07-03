bfdb2b03d124292d5c1cc4fab3e6e6c00688965c
CogyStudio <150515822+CogyStudio@users.noreply.github.com>
2024-07-03 16:45:23 +0300

    Exercise08_09 is completed

---
+++ Exercise08_09/MyClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08_09
+{
+    internal class MyClass
+    {
+        public int num;
+        public static MyClass operator +(MyClass obj1, MyClass obj2)
+        {
+            MyClass result = new MyClass();
+            result.num = obj1.num + obj2.num;
+            return result;
+        }
+        public static MyClass operator -(MyClass obj1, MyClass obj2)
+        {
+            MyClass result = new MyClass();
+            result.num = obj1.num - obj2.num;
+            return result;
+        }
+        public static MyClass operator *(MyClass obj1, MyClass obj2)
+        {
+            MyClass result = new MyClass();
+            result.num = obj1.num * obj2.num;
+            return result;
+        }
+    }
+}
---
+++ Exercise08_09/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj1 = new MyClass();
+            obj1.num = 5;
+            MyClass obj2 = new MyClass();
+            obj2.num = 3;
+            obj1 *= obj2;
+            Console.WriteLine(obj1.num);
+        }
+    }
+}
