1636beb7f4d8b1e8a7b3811aa83e57942ff71f82
CogyStudio <150515822+CogyStudio@users.noreply.github.com>
2024-07-03 15:26:03 +0300

    Exercise08_05 is completed

---
+++ Exercise08_05/MyClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08_05
+{
+    internal class MyClass
+    {
+        public int num;
+        public char symb;
+        public static bool operator true(MyClass obj)
+        {
+            if(obj.num - obj.symb <= 10 && obj.num - obj.symb >= -10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator false(MyClass obj)
+        {
+            if (obj.num - obj.symb > 10 && obj.num - obj.symb < -10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Exercise08_05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.num = 'a';
+            obj.symb = 'a';
+            while (obj)
+            {
+                Console.WriteLine(obj.symb);
+                obj.num++;
+                obj.symb++;
+                if(obj.num == 'z' + 1)
+                {
+                    obj.num = -10;
+                }
+            }
+        }
+    }
+}
