0992977d7834fb3fc56bb273c416875a8ca6c68c
CogyStudio <150515822+CogyStudio@users.noreply.github.com>
2024-07-03 16:22:18 +0300

    Exercise08_06 is completed

---
+++ Exercise08_06/MyClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08_06
+{
+    internal class MyClass
+    {
+        public int num;
+        public static bool operator true(MyClass obj)
+        {
+            switch (obj.num)
+            {
+                case 2:
+                case 3:
+                case 5:
+                case 7:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        public static bool operator false(MyClass obj)
+        {
+            if(obj.num < 1 ||  obj.num > 10)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static MyClass operator &(MyClass obj1, MyClass obj2)
+        {
+            if (obj1)
+            {
+                return obj2;
+            }
+            else
+            {
+                return obj1;
+            }
+        }
+        public static MyClass operator |(MyClass obj1, MyClass obj2)
+        {
+            if (obj1)
+            {
+                return obj1;
+            }
+            else
+            {
+                return obj2;
+            }
+        }
+
+    }
+}
---
+++ Exercise08_06/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj1 = new MyClass();
+            obj1.num = 2;
+            MyClass obj2 = new MyClass();
+            obj2.num = 1;
+            if (obj1 && obj2)
+            {
+                Console.WriteLine('A');
+            }
+            else
+            {
+                Console.WriteLine('B');
+            }
+        }
+    }
+}
