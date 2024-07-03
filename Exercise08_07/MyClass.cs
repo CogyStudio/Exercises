2c11932591e4fa379a758202015fc98f2e2e3431
CogyStudio <150515822+CogyStudio@users.noreply.github.com>
2024-07-03 16:28:39 +0300

    Exercise08_07 is completed

---
+++ Exercise08_07/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08_07
+{
+    internal class MyClass
+    {
+        public String text;
+        public static implicit operator int(MyClass obj)
+        {
+            return obj.text.Length;
+        }
+        public static implicit operator char(MyClass obj)
+        {
+            return obj.text[0];
+        }
+        public static implicit operator MyClass(int num)
+        {
+            String text = "";
+            for(int i = 0; i < num; i++)
+            {
+                text += "A";
+            }
+            MyClass result = new MyClass();
+            result.text = text;
+            return result;
+        }
+    }
+}
