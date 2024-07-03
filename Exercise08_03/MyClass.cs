8b39f82fb62352243fe76c0114038ea1241567fc
CogyStudio <150515822+CogyStudio@users.noreply.github.com>
2024-07-03 14:42:36 +0300

    Exercise08_03 is completed

---
+++ Exercise08_03/MyClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08_03
+{
+    internal class MyClass
+    {
+        public int x;
+        public int y;
+
+        public static bool operator>(MyClass obj1, MyClass obj2)
+        {
+            if(obj1.x * obj1.x + obj1.y * obj1.y > obj2.x * obj2.x + obj2.y * obj2.y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator <(MyClass obj1, MyClass obj2)
+        {
+            if (obj1.x * obj1.x + obj1.y * obj1.y < obj2.x * obj2.x + obj2.y * obj2.y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Exercise08_03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj1 = new MyClass();
+            obj1.x = 1;
+            obj1.y = 2;
+            MyClass obj2 = new MyClass();
+            obj2.x = 3;
+            obj2.y = 4;
+            Console.WriteLine(obj1 < obj2);
+        }
+    }
+}
