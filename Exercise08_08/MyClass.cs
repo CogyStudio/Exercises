22b5921b8d6d9cfe1ba24acdd992330616747ab7
CogyStudio <150515822+CogyStudio@users.noreply.github.com>
2024-07-03 16:39:53 +0300

    Exercise08_08 is completed

---
+++ Exercise08_08/MyClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08_08
+{
+    internal class MyClass
+    {
+        public int[] ints;
+        public override string ToString()
+        {
+            String result = "[";
+            for(int i = 0; i < ints.Length - 1; i++)
+            {
+                result += ints[i] + ", ";
+            }
+            result += ints[ints.Length - 1] + "]";
+            return result;
+        }
+        public static implicit operator String(MyClass obj)
+        {
+            return obj.ToString();
+        }
+        public static implicit operator int(MyClass obj)
+        {
+            int result = 0;
+            for(int i = 0; i < obj.ints.Length; i++)
+            {
+                result += obj.ints[i];
+            }
+            return result;
+        }
+        public static implicit operator MyClass(int num)
+        {
+            MyClass result = new MyClass();
+            result.ints = new int[num];
+            for(int i = 0; i < num; i++)
+            {
+                result.ints[i] = 0;
+            }
+            return result;
+        }
+    }
+}
---
+++ Exercise08_08/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            int[] nums = { 1, 2, 3 };
+            obj.ints = nums;
+            Console.WriteLine(obj.ToString());
+            Console.WriteLine((int)obj);
+            Console.WriteLine(((MyClass)10).ToString());
+        }
+    }
+}
